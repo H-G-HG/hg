b0c79b732be62f1be5c00f8511ab4fb8cc66d875
H-G-HG <ganlang211@gmail.com>
2015-08-20 00:11:42 +0800

    Update retrieveIp.c

--- retrieveIp.c
+++ retrieveIp.c
@@ -1,41 +1,27 @@
 #include<stdio.h>
-
 #include<stdlib.h>
-
 #include<string.h>
 
-
-
 int main(){
-
 FILE* fp;
-
 char* p;
-
 char returnData[64];
-
-
+char h[100][100];
+int x =0;
 fp = popen("/sbin/ifconfig eth0", "r");
-
-
 while (fgets(returnData, 64, fp) != NULL)
-
 {
 	char* tmp = returnData;
-	
-p= strtok(tmp, " ");
-	
-while(p)
-	
-{
-	printf("%s\n", p);
-    	
-p = strtok(NULL," ");
-	
-}	
+	p= strtok(tmp, " ");
+	while(p)
+	{
+	strcpy(h[x], p);
+	x++;
+    	p = strtok(NULL," ");
+	}	
 	      
-
 }
-
+printf("The IP %s\n",h[7]);
+	
 pclose(fp);
 }
